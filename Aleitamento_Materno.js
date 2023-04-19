797634b962a574aa5c5080928cbe5bd4c6e32806
Victor Alexandre Vieira de Matos <112444309+VictorAVMatos@users.noreply.github.com>
2023-04-18 21:23:06 -0300

    Add files via upload

--- Aleitamento_Materno.js
+++ Aleitamento_Materno.js
@@ -2,4 +2,4 @@ const MlDeLeite = 60;
 const QuantidadeDeMamadas = 8;
 
 const QuantidadeDeLeiteMamado = MlDeLeite * QuantidadeDeMamadas
-console.log(QuantidadeDeLeiteMamado); 
+console.log ('Hoje a Helena mamou', QuantidadeDeLeiteMamado, 'de leite materno'); 
